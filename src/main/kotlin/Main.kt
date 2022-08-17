8b62dfea6658c2555d2e7904edf4985c3065ecee
josiah <jrivera63@students.mchenry.edu>
2022-08-16 21:10:53 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("Hello my name is Josiah Rivera.")
+    println("I began coding in my freshman year of high school.")
+    println("I enjoy creating projects during my free time to practice coding")
+    println("So far, my favorite language is JavaScript because i prefer the syntax of JS over any other language I have used")
+    println("Once I get comfortable learning JavaScript, I want to learn PHP next")
+}
